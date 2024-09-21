39f5e71c903cacac75de3e3aca73d7c48ea8e9b0
Prakhar Suryavansh <ps41@tamu.edu>
2024-09-21 01:46:16 -0500

    added style

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,8 @@ module ApplicationHelper
     def toggle_sort_direction(current_sort)
         if current_sort == params[:sort]
             params[:direction] == 'asc' ? 'desc' : 'asc'
+        elsif current_sort == session[:sort]
+            session[:direction] == 'asc' ? 'desc' : 'asc'
         else
             'asc'
         end
--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,2 +1,14 @@
 module MoviesHelper
+    def highlight_column(column_name, context)
+        if session[:sort] == column_name
+            case session[:direction]
+            when 'asc'
+              context == :header ? 'highlight-header-asc' : 'highlight-data-asc'
+            when 'desc'
+              context == :header ? 'highlight-header-desc' : 'highlight-data-desc'
+            end
+        else
+            ''
+        end
+    end
 end
